799c4928409c182ec704c2e53186f845e120ffdd
Farious <fabio.i.reis@outlook.com>
2013-08-05 17:43:55 +0100

    New files and new README.md

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 pyVID2JPEG
 ==========
 
+
 Python scripts that converts any video stream into a set of JPEG files.
+This script uses OpenCV [1] bindings for python.
+
+Typical usage:
+        
+        pyVID2JPEG.py --input=test.avi --output=output/
+
+Default output folder is 'output/'
+
+
+One can install OpenCV bindings for python by:
+
+        sudo apt-get install python-numpy python-opencv 
+
+or by compiling from source and selecting to compile python bindings.
+
+[1] http://opencv.org/
+
+
+
+*******************************************************************************
+Fabio Reis  
+[info - at - fabioreis.net]  
+Please email me if you find bugs, or have suggestions or questions!
---
+++ pyVID2JPEG.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+import cv2, getopt, sys, urllib, time
+
+def main():
+# input arguments
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"h:io", ["help","input=","output="])
+        if opts == []:
+            usage()
+            sys.exit('')
+    except getopt.GetoptError:
+        usage()
+    
+    inputF = ''
+    outputF = 'output/'
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        if o in ("-i", "--input"):
+            inputF = str(a)
+        if o in ("-o", "--output"):
+            outputF = str(a)
+
+    if inputF == '':
+        usage()
+        sys.exit()
+
+# strips video stream
+    stripVideo(inputF, outputF)
+
+def stripVideo(iF, oD):
+# strips video stream
+    try:
+        cap = cv2.VideoCapture(iF)
+        if not cap.isOpened():
+            print 'Couldn\'t open file, please confirm that it is a video file.'
+            raise IOError
+    except:
+        sys.exit()
+
+    name = iF.split('.')[0].split('/')[-1]
+    print 'Video file opened, starting strip of the video.'
+    
+    imOK, image = cap.read()
+    img_counter = 0
+    residual = 0
+    while imOK:
+        img_counter += 1
+        cv2.imwrite(oD + name + '_{0:05d}.jpg'.format(img_counter), image)
+        
+        q, r = divmod(img_counter, 100)
+        if r == 0:
+            print 'Processed {} images.'.format(img_counter)
+        imOK, image = cap.read()
+
+    print 'Done, {} image files were created.'.format(img_counter)
+
+def usage():
+
+    print ' -------------------------------------------------------------------------'
+    print ' Fabio Reis (info - at - fabioreis.net),  2013'
+    print ' '
+    print ' Turns any video stream into a group of JPEG individual images. ' 
+    print ' '
+    print ' Typical usage:'
+    print ' pyVID2JPEG.py --input=test.avi --output=output/'
+    print ' '
+    print ' -------------------------------------------------------------------------'
+    sys.exit(' ')
+
+#-------------------------------
+if __name__ == "__main__":
+    main()
