d4638705ee8c76374291d6170594d429a232bdaf
Fábio Reis <fabio.imr.reis@gmail.com>
2015-02-05 22:20:54 +0000

    Merged vid2jpeg with jpeg2vid.

--- pyVID2JPEG.py
+++ pyVID2JPEG.py
@@ -19,30 +19,56 @@ import cv2, getopt, sys, urllib, time
 
 def main():
 # input arguments
-    try:
-        opts, args = getopt.getopt(sys.argv[1:],"h:io", ["help","input=","output="])
-        if opts == []:
-            usage()
-            sys.exit('')
-    except getopt.GetoptError:
-        usage()
+  try:
+    opts, args = getopt.getopt(sys.argv[1:],"h:vcfrio", ["help","i2v=","fourcc=", "fps=", "res=", "input=","output="])
+    if opts == []:
+      usage()
+      sys.exit('')
+  except getopt.GetoptError:
+    usage()
+  
+  inputF = ''
+  inputFiles = ''
+  outputF = 'output/'
+  
+  i2v = False
+  imgType = ".jpg"
+  for o, a in opts:
+    if o in ("-v", "--i2v"):
+      i2v = True
+      imgType = str(a)
+    if o in ("-c", "--fourcc"):
+      fourcc = int(a)
+    if o in ("-f", "--fps"):
+      fps = int(a)
+    if o in ("-r", "--res"):
+      continue  
+    if o in ("-h", "--help"):
+      usage()
+    if o in ("-i", "--input"):
+      inputF = str(a)
+    if o in ("-o", "--output"):
+      outputD = str(a)
     
-    inputF = ''
-    outputF = 'output/'
+  if i2v:
+    inputFiles = glob.glob(inputF + "*." + imgType)
+    inputFiles.sort()
+    outputD = inputF + 'video.avi'
 
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            usage()
-        if o in ("-i", "--input"):
-            inputF = str(a)
-        if o in ("-o", "--output"):
-            outputD = str(a)
+  if inputF == '':
+    usage()
+    sys.exit()
 
-    if inputF == '':
-        usage()
-        sys.exit()
-
-# strips video stream
+  if i2v:
+    print "Doing i2v"
+    #fourcc = cv2.cv.CV_FOURCC(*'FMP4')
+    #fps = 5.0
+    fourcc = cv2.cv.CV_FOURCC(*'MJPG')
+    img = cv2.imread(inputFiles[0])
+    imageSize = (img.shape[1], img.shape[0])
+    mergeVideo(inputFiles, outputD, fourcc, fps, imageSize)
+  else:
+    # strips video stream
     stripVideo(inputF, outputD)
 
 def stripVideo(inputF, outputD):
@@ -72,12 +98,44 @@ def stripVideo(inputF, outputD):
 
     print 'Done, {} image files were created.'.format(img_counter)
 
+def mergeVideo(inputF, outputF, fourcc, fps, frameSize):
+# merges consecutive image files into a video stream
+  try:
+    outputVideo = cv2.VideoWriter(outputF, fourcc, fps, frameSize)
+    if not outputVideo.isOpened():
+      print 'Couldn\'t create video output file.'
+      raise IOErro
+  except:
+    print "Something happened."
+    sys.exit()
+  
+  img_counter = 0
+  for imgFile in inputF:
+    try:
+      img = cv2.imread(imgFile)
+      if img == None:
+        raise IOError
+    except:
+      print 'Couldn\'t open image file: ' + imgFile
+      print 'Skipping image file.'
+
+    outputVideo.write(img)
+
+    # print at each 100's image processed
+    q, r = divmod(img_counter, 100)
+    if r == 0:
+      print 'Processed {} images.'.format(img_counter)
+    
+    img_counter += 1
+  del outputVideo
+
 def usage():
 
     print ' -------------------------------------------------------------------------'
     print ' Fabio Reis (info - at - fabioreis.net),  2013'
     print ' '
-    print ' Turns any video stream into a group of JPEG individual images. ' 
+    print ' Turns any video stream into a group of JPEG individual images and vice- ' 
+    print ' -versa.'
     print ' '
     print ' Typical usage:'
     print ' pyVID2JPEG.py --input=test.avi --output=output/'
